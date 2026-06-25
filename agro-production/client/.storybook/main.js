6b010481165bdfc67931331e2f464336282b2082
auraroom <auraroom001@gmail.com>
2026-06-25 18:39:48 +0100

    fix(deps): remove Storybook to resolve React 19 peer dependency conflict
    Removes Storybook 7 devDependencies and configuration that were incompatible
    with React 19. Storybook 7 declares peer support for React 16-18 only.
    
    Resolves #469

--- agro-production/client/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  addons: ["@storybook/addon-essentials"],
-};
--- agro-production/client/src/components/NavBar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import NavBar from "./NavBar";
-
-export default {
-  title: "Components/NavBar",
-  component: NavBar,
-};
-
-export const Default = () => <NavBar />;
